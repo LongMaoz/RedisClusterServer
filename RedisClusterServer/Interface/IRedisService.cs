63d13ab34cbfd8980f0f2b0172f0240f910a2538
LongMaoz <287175199@qq.com>
2024-01-16 14:29:17 +0800

    实现Hash类型的Set 和 String类型的Get和Set

--- RedisClusterServer/Interface/IRedisService.cs
+++ RedisClusterServer/Interface/IRedisService.cs
@@ -1,5 +1,6 @@
 ﻿using StackExchange.Redis;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace RedisClusterServer.Interface
 {
     public interface IRedisService
     {
+
+        #region Hash
 
         /// <summary>
         /// 获取Hash
@@ -32,5 +35,60 @@ namespace RedisClusterServer.Interface
         /// <returns></returns>
         public Task<IEnumerable<T>> HashGetAllParallelAsync<T>(string hashKey) where T : class;
 
+        /// <summary>
+        /// HashSet
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="hashkey"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Task<bool> HashSetAsync<T>(string hashkey, string key, T value) where T : class;
+
+
+        /// <summary>
+        /// HashSet 批量插入
+        /// </summary>
+        /// <param name="hashkey"></param>
+        /// <param name="entries"></param>
+        /// <returns></returns>
+        public Task HashSetAsync(string hashkey, HashEntry[] entries);
+
+        #endregion 
+
+        #region String
+
+        /// <summary>
+        /// 单独插入StringKey
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public Task<bool> StringSetAsync(string key, string val);
+
+        /// <summary>
+        /// 批量插入StringKey 批量操作需要指定Hash Tag
+        /// </summary>
+        /// <param name="pairs"></param>
+        /// <returns></returns>
+        public Task<bool> StringSetAsync(Dictionary<string, string> dic);
+
+        /// <summary>
+        /// 批量并行插入StringKey 批量操作需要指定Hash Tag
+        /// </summary>
+        /// <param name="dic"></param>
+        /// <returns></returns>
+        public Task<bool> StringSetParalleAsync(ConcurrentDictionary<string, string> dic);
+
+        /// <summary>
+        /// 获取String
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public Task<T> StringGetAsync<T>(string key);
+
+
+        #endregion
     }
 }
--- RedisClusterServer/Service/RedisService.cs
+++ RedisClusterServer/Service/RedisService.cs
@@ -8,6 +8,7 @@ using Newtonsoft.Json;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace RedisClusterServer.Service
 {
@@ -29,6 +30,8 @@ namespace RedisClusterServer.Service
             _database = RedisConnectionFactory.ConnFactory.Value.GetConnection(entities);
         }
 
+        #region Hash
+
         public async Task<IEnumerable<T>> HashGetAllAsync<T>(string hashKey) where T : class
         {
             HashEntry[] hashEntries =  await _database.HashGetAllAsync(hashKey, CommandFlags.PreferReplica);
@@ -45,6 +48,48 @@ namespace RedisClusterServer.Service
         {
             return await _database.HashGetAllAsync(hashKey, CommandFlags.PreferReplica);
         }
+
+        public async Task<bool> HashSetAsync<T>(string hashkey,string key,T value) where T : class
+        {
+            string json = JsonConvert.SerializeObject(value);
+            return await _database.HashSetAsync(hashkey,key,json);
+        }
+
+        public async Task HashSetAsync(string hashkey, HashEntry[] entries)
+        {
+            await _database.HashSetAsync(hashkey, entries);
+        }
+
+        #endregion
+
+        #region String
+
+        public async Task<bool> StringSetAsync(string key, string val)
+        {
+            return await _database.StringSetAsync(key,val);
+        }
+
+        public async Task<bool> StringSetAsync(Dictionary<string, string> dic)
+        {
+            var pairs = dic.Select(x => new KeyValuePair<RedisKey, RedisValue>(x.Key, x.Value)).ToArray();
+            return await _database.StringSetAsync(pairs);
+        }
+
+        public async Task<bool> StringSetParalleAsync(ConcurrentDictionary<string, string> dic)
+        {
+            var pairs = dic.AsParallel().Select(x => new KeyValuePair<RedisKey, RedisValue>(x.Key, x.Value)).ToArray();
+            return await _database.StringSetAsync(pairs);
+        }
+
+        public async Task<T> StringGetAsync<T>(string key)
+        {
+            string val = await _database.StringGetAsync(key,CommandFlags.PreferReplica);
+            if (typeof(T) == typeof(string))
+                return (T)Convert.ChangeType(val, typeof(T));
+            return JsonConvert.DeserializeObject<T>(val);
+        }
+
+        #endregion
 
     }
 }
